f3884ffd5a1ac3ce403722093b29da988878f571
Felix Albrecht <felix.albrecht.priv@gmail.com>
2018-10-22 21:52:32 +0200

    added pdf form extract function

--- batchPdfFormFill.py
+++ batchPdfFormFill.py
@@ -1,8 +1,9 @@
 from tkinter import filedialog, Frame, Button, Entry, Tk, StringVar, E, Label
-from pandas import read_excel
-import pypdftk 
+from pandas import read_excel, DataFrame, ExcelFile, ExcelWriter
+import pypdftk
 from operator import itemgetter
-
+from os import listdir
+from collections import defaultdict
 
 # class for window content
 class CoreGui(Frame):
@@ -10,6 +11,8 @@ class CoreGui(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
 
+        self.fillPdflabel = Label(master, text="PDF Formulare befüllen mit XLSX Daten",
+                                  font='Helvetica 18 bold')
         # button pdf select
         self.selectPdfFormBut = Button(master, text="PDF Formular",
                                        command=self.selectPdfForm)
@@ -23,7 +26,7 @@ class CoreGui(Frame):
                                          command=self.selectTargetFolder)
 
         # button create multiple pdfs
-        self.createLottaPdfButton = Button(master, text='LOS',
+        self.createLottaPdfButton = Button(master, text='PDF Formulare befüllen',
                                            command=self.createMultiplePDF)
         
         self.IDlabel = Label(master, text="ID-Spalten (ID1,ID2,ID3)")
@@ -51,18 +54,51 @@ class CoreGui(Frame):
         self.inputTarget = Entry(master, width=40, state='readonly',
                                  textvariable=self.inputTargetPath)
 
+    # ---------------------------------------------------------------------------------------
+        self.extractPdflabel = Label(master, text="PDF Formulare zu XLSX extrahieren",
+                                     font='Helvetica 18 bold')
+
+        # button input folder select
+        self.selectBaseButton = Button(master, text='PDF Ordner',
+                                         command=self.selectBaseFolder)
+        # input path folder
+        self.inputBasePath = StringVar()
+        self.inputBase = Entry(master, width=40, state='readonly',
+                               textvariable=self.inputBasePath)
+
+        # button input folder select
+        self.selectOutputExcel = Button(master, text='Ausgabe Excel',
+                                        command=self.selectOutExcel)
+        # text field read only for Excel path
+        self.outputExcelPath = StringVar()
+        self.outputExcel = Entry(master, width=40, state='readonly',
+                                 textvariable=self.outputExcelPath)
+
+        # button extract multiple pdfs
+        self.extractLottaPdfButton = Button(master, text='PDF zu XLSX extrahieren',
+                                            command=self.extractMultiplePDF)
+        
+    # ---------------------------------------------------------------------------------------
         # initialize graphical objects in frame (order in grid)
-        self.selectPdfFormBut.grid(row=0, column=0, sticky=E)
-        self.selectExcelButton.grid(row=1, column=0, sticky=E)
-        self.IDlabel.grid(row=2,column=0,sticky=E)
-        self.selectTargetButton.grid(row=3, column=0, sticky=E)
+        self.fillPdflabel.grid(row=0,column=0,columnspan=2)
+        self.selectPdfFormBut.grid(row=1, column=0, sticky=E)
+        self.selectExcelButton.grid(row=2, column=0, sticky=E)
+        self.IDlabel.grid(row=3,column=0,sticky=E)
+        self.selectTargetButton.grid(row=4, column=0, sticky=E)
+        self.createLottaPdfButton.grid(row=5, column=1, sticky=E)
 
-        self.inputPdf.grid(row=0, column=1, sticky=E)
-        self.inputExcel.grid(row=1, column=1, sticky=E)
-        self.inputID.grid(row=2, column=1, sticky=E)
-        self.inputTarget.grid(row=3, column=1, sticky=E)
+        self.extractPdflabel.grid(row=6,column=0,columnspan=2)
+        self.selectBaseButton.grid(row=7,column=0,sticky=E)
+        self.selectOutputExcel.grid(row=8,column=0,sticky=E)
+        self.extractLottaPdfButton.grid(row=9, column=1, sticky=E)
 
-        self.createLottaPdfButton.grid(row=4, column=1, sticky=E)
+        self.inputPdf.grid(row=1, column=1, sticky=E)
+        self.inputExcel.grid(row=2, column=1, sticky=E)
+        self.inputID.grid(row=3, column=1, sticky=E)
+        self.inputTarget.grid(row=4, column=1, sticky=E)
+        self.inputBase.grid(row=7, column=1, sticky=E)
+        self.outputExcel.grid(row=8, column=1, sticky=E)
+
 
     def selectPdfForm(self):
         filename = filedialog.askopenfilename(initialdir="./",
@@ -81,6 +117,17 @@ class CoreGui(Frame):
         foldername = filedialog.askdirectory()
         self.inputTargetPath.set(foldername)
 
+    def selectBaseFolder(self):
+        foldername = filedialog.askdirectory()
+        self.inputBasePath.set(foldername)
+
+    def selectOutExcel(self):
+        filename = filedialog.asksaveasfilename(initialdir='./',
+                                                title='Speichern unter',
+                                                filetypes=(('Excel Dateien', '*.xlsx'),
+                                                         ('Alle Dateien', '*.*')))
+        self.outputExcelPath.set(filename)
+
     def createMultiplePDF(self):
         #create dataframe from excel upload
         df = read_excel(self.inputExcel.get())
@@ -98,8 +145,26 @@ class CoreGui(Frame):
                               OutPDF+'_'+IDname+'_'+str(count)+'.pdf'+'"', flatten=False, need_appearances=True)
             print('Created: '+OutPDF+'_'+IDname+'_'+str(count)+'.pdf')
 
+    def extractMultiplePDF(self):
+        pdfPath = self.inputBase.get()
+        xlsxOut = self.outputExcel.get()
+        pdfFiles = [x for x in listdir(pdfPath) if ".pdf" in x.lower()]
+        rows = []
+        for el in pdfFiles:
+            raw = pypdftk.dump_data_fields(pdfPath+'/'+el)
+            rows = rows + [(x['FieldName'], x['FieldValue']) for x in raw]
+        drows = defaultdict(list)
+        for k, v in rows:
+            drows[k].append(v)
+        df = DataFrame.from_dict(drows)
+        print(df)
+        writer = ExcelWriter(self.outputExcel.get())
+        df.to_excel(writer, 'Sheet1', index=False)
+        writer.save()
+
+
 root = Tk()
-root.title('Massengenerierung PDF aus Excel')
+root.title('Massenverarbeitung von PDF-Formularen')
 #root.configure(background='gray1')
 my_gui = CoreGui(root)
 root.mainloop()
